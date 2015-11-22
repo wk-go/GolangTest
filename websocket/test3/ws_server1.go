3328a5538a4c7c6fb5e8f5c2edb82494323da3e2
walkskyer <walkskyker@qq.com>
2015-11-22 21:46:09 +0800

    websocket测试程序test3:为转发程序增加channel控制使用select选择控制发送数据

--- websocket/test3/ws_server1.go
+++ websocket/test3/ws_server1.go
@@ -9,6 +9,7 @@ import (
 	"container/list"
 	"reflect"
 	"encoding/json"
+	"time"
 )
 
 type Msg_data struct{
@@ -20,22 +21,45 @@ type Msg_data struct{
 }
 
 type Client struct{
-	ws 				*websocket.Conn
 	client_id 		string
 	nickname 		string
+	ws 				*websocket.Conn
 	msg 			*list.List
 }
 
 type Client_list struct{
-	list 			*list.List
+	list 			*list.List		//客户端列表
+	msg_list		*list.List		//群发消息列表
+	msg_ch			chan int		//群发消息开关
 }
 
 var c_list *Client_list
-//
+//添加客户端
 func (cl *Client_list)Append(data interface{}) (*list.Element){
 	return cl.list.PushBack(data)
 }
-//发送消息
+
+//处理客户端发送的消息
+func (cl *Client_list)msg_handle(msg string, ws *websocket.Conn){
+	var msg_tmp interface{}
+	if err := json.Unmarshal([]byte(msg), &msg_tmp); err != nil{
+		fmt.Println(err)
+	}
+	if msg_data, ok := msg_tmp.(map[string]interface{}); ok{
+		switch msg_data["_t"] {
+		case "normal":
+				if msg_data["to_user"] == ""{
+					cl.msg_list.PushBack(msg_data)
+					cl.msg_ch <- 1
+				}
+		case "set":
+
+		}
+	}
+}
+
+
+//群发消息
 func (cl *Client_list)SendMsg(msg string) (error){
 	if cl.list.Len() > 0{
 		for element := cl.list.Front(); element != nil;  {
@@ -55,6 +79,31 @@ func (cl *Client_list)SendMsg(msg string) (error){
 	}
 	return nil
 }
+//处理群发消息
+func (cl *Client_list)SendNormal() (error){
+	if cl.msg_list.Len() > 0 {
+		e := cl.msg_list.Front();
+		if msg_data, ok := e.Value.(map[string]interface{}); ok{
+			if msg, err := json.Marshal(msg_data); err == nil{
+				cl.SendMsg(string(msg))
+			}
+		}
+		cl.msg_list.Remove(e)
+	}
+	return nil
+}
+
+//群发go程
+func (cl *Client_list) start_mass(){
+	for{
+		select {
+		case <-cl.msg_ch:
+			cl.SendNormal()
+		default:
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
 
 //接收客户端连接请求
 func Accept(ws *websocket.Conn) {
@@ -68,25 +117,18 @@ func Accept(ws *websocket.Conn) {
 			break
 		}
 		fmt.Println("Received from client: " + reply)
-		msg_handle(reply, ws)
-	}
-}
-
-//处理客户端发送的消息
-func msg_handle(msg string, ws *websocket.Conn){
-	var msg_tmp interface{}
-	if err := json.Unmarshal([]byte(msg), &msg_tmp); err != nil{
-		fmt.Println(err)
-	}
-	if msg_data, ok := msg_tmp.(map[string]interface{}); ok{
-		if(msg_data["_t"] == "normal" && msg_data["to_user"] == ""){
-			go c_list.SendMsg(msg)
-		}
+		c_list.msg_handle(reply, ws)
 	}
 }
 
 func main() {
-	c_list = &Client_list{list:list.New()}
+	c_list = &Client_list{
+		list:		list.New(),
+		msg_list:	list.New(),
+		msg_ch:		make(chan int,100),
+	}
+
+	go c_list.start_mass()
 
 	http.Handle("/", websocket.Handler(Accept))
 	fmt.Println("start serving")
