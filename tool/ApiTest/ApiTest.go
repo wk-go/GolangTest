c120ba962bbd8954159cb1b60b99d876687e8367
walkskyer <walkskyer@qq.com>
2015-12-08 14:36:06 +0800

    ApiTest:模板添加模块化支持

--- tool/ApiTest/ApiTest.go
+++ tool/ApiTest/ApiTest.go
@@ -1,7 +1,7 @@
 //api test
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
 	"log"
@@ -17,44 +17,72 @@ var AppInfo = map[string]string{
 	"assets":"./web/assets",
 	"views":"./web/views",
 }
+var Config = map[string]interface{}{
+	"tpml":map[string]string{
+		"header":"./web/views/header.html",
+		"footer":"./web/views/footer.html",
+	},
+}
 //路由表
 var UrlRoute = map[string]http.HandlerFunc{
 	"/":index,
 }
-func main(){
+func main() {
 	//设置静态文件
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir(AppInfo["assets"]))))
 	//绑定路由
-	for key,value := range UrlRoute{
-		http.Handle(key,value)
+	for key, value := range UrlRoute {
+		http.Handle(key, value)
 	}
 	log.Println("Server is starting")
 	log.Fatal(http.ListenAndServe("localhost:10000", nil))
 }
 //读取视图文件
-func ReadView(viewName string)(string)  {
+func ReadView(viewName string) (string) {
 	var content []byte
 	var err error
-	if content, err = ioutil.ReadFile(fmt.Sprintf("%v/%v.html",AppInfo["views"], viewName)); err!=nil{
+	if content, err = ioutil.ReadFile(fmt.Sprintf("%v/%v.html", AppInfo["views"], viewName)); err != nil {
 		log.Println(err)
 		return ""
 	}
 	return string(content)
 }
 //渲染视图
-func RenderView(w io.Writer,viewName string, data map[string]interface{}){
+func RenderView(w io.Writer, viewName string, data map[string]interface{}) {
 	//绑定默认数据
-	if _,ok :=data["appInfo"];!ok{
-		data["appInfo"]=AppInfo
+	if _, ok := data["appInfo"]; !ok {
+		data["appInfo"] = AppInfo
+	}
+	if _, ok := data["config"]; !ok {
+		data["config"] = Config
 	}
 
+	//渲染默认页头
+	if _, ok := data["header"]; !ok {
+		viewCnt := ReadView("header")
+		tmpl, err := template.New(viewName).Parse(viewCnt)
+		if err != nil { panic(err) }
+		err = tmpl.Execute(w, data)
+		if err != nil { panic(err) }
+	}
+
+	//渲染业务视图
 	viewCnt := ReadView(viewName)
-	tmpl,err := template.New(viewName).Parse(viewCnt)
+	tmpl, err := template.New(viewName).Parse(viewCnt)
 	if err != nil { panic(err) }
 	err = tmpl.Execute(w, data)
 	if err != nil { panic(err) }
+
+	//渲染默认页脚
+	if _, ok := data["footer"]; !ok {
+		viewCnt := ReadView("footer")
+		tmpl, err := template.New(viewName).Parse(viewCnt)
+		if err != nil { panic(err) }
+		err = tmpl.Execute(w, data)
+		if err != nil { panic(err) }
+	}
 }
 //首页
-func index(w http.ResponseWriter, r *http.Request){
-	RenderView(w,"index",map[string]interface{}{})
+func index(w http.ResponseWriter, r *http.Request) {
+	RenderView(w, "index", map[string]interface{}{})
 }
