5076b78fe39d791ab813a8457f26da632c4dc427
walkskyer <walkskyer@qq.com>
2016-04-28 14:56:59 +0800

    时间函数测试

--- module/crypto/time/test1.go
+++ module/crypto/time/test1.go
@@ -5,7 +5,15 @@ import(
 )
 func main() {
 	now := time.Now()
+	fmt.Println("-------local----------")
+	fmt.Println("time.Location():", now.Location())
 	fmt.Println("time.Now():", now)
 	datesum := now.YearDay()%256
+	fmt.Println("datesum:", datesum)
+	fmt.Println("-------utc----------")
+	nowUTC := now.UTC()
+	fmt.Println("time.Now():", nowUTC.Location())
+	fmt.Println("time.Now():", nowUTC)
+	datesum = now.YearDay()%256
 	fmt.Println("datesum:", datesum)
 }
