e8b3468cb33d927f86e74b2b854313219dd6bdcb
walkskyer <walkskyer@qq.com>
2017-01-20 12:19:50 +0800

    feat(app/wxBot):等待登录功能完成

--- app/wxBot/test.go
+++ app/wxBot/test.go
@@ -9,30 +9,40 @@ import (
     "io/ioutil"
     "regexp"
     "github.com/skip2/go-qrcode"
+    "strings"
 )
 
 func main() {
     b := &WxBot{}
     b.GetUuid()
     b.GenQrCode()
-
+    b.Wait4Login()
 }
+
+const (
+    UNKONWN = "unkonwn"
+    SUCCESS = "200"
+    SCANED = "201"
+    TIMEOUT = "408"
+)
 
 type WxBot struct {
-    BaseUri string
-    Client http.Client
-    Uuid string
-    LoginQr []byte
+    base_uri     string
+    Client       http.Client
+    uuid         string
+    LoginQr      []byte
+    redirect_uri string
+    base_host    string
 }
 func (this *WxBot) GetUuid() bool{
-    targetUrl := "https://login.weixin.qq.com/jslogin"
+    urlStr := "https://login.weixin.qq.com/jslogin"
     params := url.Values{
         "appid": []string{"wx782c26e4c19acffb"},
         "fun": []string{"new"},
         "lang": []string{"zh_CN"},
         "_": []string{string(int(time.Now().Unix()) * 1000 + rand.Int())},
     }
-    resp, err :=this.Client.PostForm(targetUrl,params)
+    resp, err :=this.Client.PostForm(urlStr,params)
     if err != nil {
         // handle error
     }
@@ -52,14 +62,14 @@ func (this *WxBot) GetUuid() bool{
     }
 
     if len(submatch) == 3 {
-        this.Uuid = string(submatch[2])
+        this.uuid = string(submatch[2])
         return string(submatch[1]) == "200"
     }
     return false
 }
 
 func (this *WxBot) GenQrCode(){
-    urlStr := "https://login.weixin.qq.com/l/"+this.Uuid
+    urlStr := "https://login.weixin.qq.com/l/"+this.uuid
     /*var png []byte
     png, err := qrcode.Encode(urlStr, qrcode.Medium, 256)
     if err != nil{
@@ -71,3 +81,76 @@ func (this *WxBot) GenQrCode(){
 
     }
 }
+func (this *WxBot) Wait4Login() string{
+    LOGIN_TEMPLATE := "https://login.weixin.qq.com/cgi-bin/mmwebwx-bin/login?tip=%d&uuid=%s&_=%v"
+    tip :=1
+
+    try_later_secs := 1*time.Second
+    MAX_RETRY_TIMES := 10
+
+    code := UNKONWN
+
+    retry_time := MAX_RETRY_TIMES
+    for ;retry_time > 0; {
+        urlStr := fmt.Sprintf(LOGIN_TEMPLATE, tip, this.uuid, time.Now().Unix())
+        fmt.Printf(":::url::: %s \n",urlStr)
+        code, data := this.do_request(urlStr)
+        switch {
+            case code == SCANED :
+                fmt.Printf("[INFO] Please confirm to login.\n")
+                tip = 0
+            case code == SUCCESS :
+                // 确认登录成功
+                re, err := regexp.Compile("window.redirect_uri=\"(.+?)\";")
+                if err != nil {
+
+                }
+                fmt.Println(":::body:::",string(data))
+                param := re.FindSubmatch(data)
+                fmt.Println(":::param:::",param)
+
+                RedirectUri := string(param[1]) + "&fun = new"
+                this.redirect_uri = RedirectUri
+                this.base_uri = RedirectUri[:strings.LastIndex(RedirectUri, "/")]
+                temp_host := this.base_uri[8:]
+                this.base_host = temp_host[:strings.LastIndex(temp_host, "/")]
+                fmt.Printf("[Success] WeChat login:%v\n", this)
+                return code
+            case code == TIMEOUT :
+                fmt.Printf("[ERROR] WeChat login timeout.retry in % s secs later...\n", try_later_secs)
+                tip = 1  //重置
+                retry_time -= 1
+                time.Sleep(try_later_secs)
+            default:
+                fmt.Printf("[ERROR] WeChat login exception return_code = %s.retry in %s secs later...\n", code, try_later_secs)
+                tip = 1
+                retry_time -= 1
+                time.Sleep(try_later_secs)
+        }
+    }
+    return code
+}
+
+func (this *WxBot) do_request(url string) (string, []byte) {
+    r, err := this.Client.Get(url)
+    if err != nil {
+
+    }
+    defer r.Body.Close()
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+
+    }
+    fmt.Println(":::body:::",string(body))
+    re, err := regexp.Compile("window.code=([0-9]+?);")
+    if err != nil {
+
+    }
+    param := re.FindSubmatch(body)
+
+    fmt.Println(":::param:::", param)
+    code := string(param[1])
+
+    return code, body
+}
+
