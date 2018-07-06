28ea986229a54051bcbfcfe77de40613694745dd
walkskyer <walkskyer@qq.com>
2018-07-06 13:11:50 +0800

    qt test01 add button

--- module/qt/test01/main.go
+++ module/qt/test01/main.go
@@ -1,14 +1,24 @@
 package main
 import(
 	"github.com/therecipe/qt/widgets"
+	"github.com/therecipe/qt/core"
 	"os"
 )
 
 
 func main(){
 	widgets.NewQApplication(len(os.Args), os.Args)
+
 	window := widgets.NewQMainWindow(nil, 0)
+	window.Resize(core.NewQSize2(600,400))
 	window.SetWindowTitle("hello world");
 	window.Show()
+
+	//button
+	button := widgets.NewQPushButton(window)
+	button.Move(core.NewQPoint2(100,100))
+	button.SetText("Click Me!")
+	button.Show()
+
 	widgets.QApplication_Exec()
 }
