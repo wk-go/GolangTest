ca6868cd3d110db2080ec37aae628635a8f60508
walkskyer <walkskyer@qq.com>
2018-07-11 11:02:47 +0800

    定时任务demo功能完善

--- basic/goroutine/timer.go
+++ basic/goroutine/timer.go
@@ -1,33 +1,39 @@
 package main
+
 // 定时器测试
 import (
-    "time"
-    "fmt"
+	"time"
+	"log"
 )
 
 var input = make(chan string, 10)
 
-func main(){
-    demo()
+func main() {
+	demo()
 }
 
 func demo() {
-    t1 := time.NewTimer(time.Second * 5)
-    t2 := time.NewTimer(time.Second * 10)
+	t1 := time.NewTimer(time.Second * 6)
+	t2 := time.NewTimer(time.Second * 10)
+	t3 := time.NewTimer(time.Second * 2)
 
-    for {
-        select {
-        case msg := <- input:
-            fmt.Println(msg)
+	for {
+		select {
+		case msg := <-input:
+			log.Println(msg)
 
-        case <-t1.C:
-            fmt.Println("5s timer")
-            t1.Reset(time.Second * 5)
+		case <-t1.C:
+			log.Println("[06s] timer")
+			t1.Reset(time.Second * 6)
 
-        case <-t2.C:
-            fmt.Println("10s timer")
-            t2.Reset(time.Second * 10)
-        }
-    }
+		case <-t2.C:
+			log.Println("[10s] timer")
+			t2.Reset(time.Second * 10)
+
+		case <-t3.C:
+			input <- "[02s] timer:set input"
+			t3.Reset(time.Second * 2)
+		}
+
+	}
 }
-
