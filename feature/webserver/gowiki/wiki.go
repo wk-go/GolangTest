9da7b7f86c421f32e8dc21a8d3d0a971d6cdb509
walkskyer <walkskyer@qq.com>
2018-08-30 11:15:55 +0800

    wiki basic

--- feature/webserver/gowiki/wiki.go
+++ feature/webserver/gowiki/wiki.go
@@ -8,12 +8,12 @@ import(
 
 type Page struct {
 	Title	string
-	Body 	[]byte
+	Body 	string
 }
 
 func (p *Page)save() error{
 	filename := p.Title + ".txt"
-	return ioutil.WriteFile(filename, p.Body, 0600)
+	return ioutil.WriteFile(filename, []byte(p.Body), 0600)
 }
 
 func loadPage(title string)(*Page, error){
@@ -22,7 +22,7 @@ func loadPage(title string)(*Page, error){
 	if err != nil{
 		return nil, err
 	}
-	return &Page{Title:title, Body:body}, nil
+	return &Page{Title:title, Body:string(body)}, nil
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request){
@@ -30,10 +30,37 @@ func viewHandler(w http.ResponseWriter, r *http.Request){
 	p, _ := loadPage(title)
 	fmt.Fprintf(w,"<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
+
+func editHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/edit/"):]
+	p,err := loadPage(title)
+	if err != nil {
+		p = &Page{Title:title}
+	}
+	fmt.Fprintf(w,"<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request){
+	title := r.URL.Path[len("/save/"):]
+	body :=r.FormValue("body")
+	p := &Page{Title: title, Body: body}
+	err := p.save()
+	if err != nil{
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
 func main(){
-	p1 := &Page{Title: "test", Body:[]byte("This is a sample Page")}
+	p1 := &Page{Title: "test", Body:"This is a sample Page"}
 	p1.save()
 
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
