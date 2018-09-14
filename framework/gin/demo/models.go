2c4986fe9bc4de3e1db8817c7136faaa51e08c4e
walkskyer <walkskyer@qq.com>
2018-09-14 16:13:54 +0800

    gin test add model

---
+++ framework/gin/demo/models.go
@@ -0,0 +1,21 @@
+package main
+
+import "github.com/jinzhu/gorm"
+
+type Blog struct {
+	gorm.Model
+	Title string `json:"title"`
+	Content string `json:"content"`
+	CatId	string 	`json:"cat_id"`
+}
+
+type Category struct{
+	gorm.Model
+	Name string `json:"name"`
+}
+
+type User struct {
+	gorm.Model
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
