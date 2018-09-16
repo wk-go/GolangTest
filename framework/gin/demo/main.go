360bb65e57958324a25147f8e00bad66735f180c
walkskyer <walkskyer@qq.com>
2018-09-16 12:59:57 +0800

    gin add db

--- framework/gin/demo/main.go
+++ framework/gin/demo/main.go
@@ -1,73 +1,90 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/gin-contrib/sessions"
-	"github.com/gin-contrib/sessions/cookie"
-	"path/filepath"
-	"github.com/gin-contrib/multitemplate"
-	"strings"
+    "github.com/gin-gonic/gin"
+    "github.com/gin-contrib/sessions"
+    "github.com/gin-contrib/sessions/cookie"
+    "path/filepath"
+    "github.com/gin-contrib/multitemplate"
+    "strings"
+
+    "github.com/jinzhu/gorm"
+    _ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
+var DB *gorm.DB
+
 func main() {
-	r := gin.Default()
-	render := loadTemplates("views/admin")
-	render = frontTemplates("views/front", render)
-	r.HTMLRender = render
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	frontCtrl := &FrontController{}
-	frontGroup := r.Group("/")
-	{
-		frontGroup.GET("", frontCtrl.Index)
-		frontGroup.GET("view", frontCtrl.View)
-	}
+    //db init
 
-	adminCtrl := &AdminController{}
-	adminGroup := r.Group("/admin")
-	store := cookie.NewStore([]byte("secret"))
-	adminGroup.Use(sessions.Sessions("mysession", store))
-	{
-		adminGroup.Static("/assets", "views/admin/assets")
-		adminGroup.GET("",adminCtrl.Index)
-		adminGroup.GET("/login",adminCtrl.Login)
-		adminGroup.POST("/login",adminCtrl.Login)
-		adminGroup.GET("/logout",adminCtrl.Logout)
-		adminGroup.GET("/statistics",adminCtrl.Statistics)
-		adminGroup.GET("/session-test",adminCtrl.SessionTest)
-	}
-	r.Run() // listen and serve on 0.0.0.0:8080
+    DB, err := gorm.Open("sqlite3", "cache/my.db")
+    if err != nil {
+        panic(err)
+    }
+    defer DB.Close()
+
+    // Migrate the schema
+    DB.AutoMigrate(&User{}, &Article{}, &Category{})
+
+    r := gin.Default()
+    render := loadTemplates("views/admin")
+    render = frontTemplates("views/front", render)
+    r.HTMLRender = render
+
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "pong",
+        })
+    })
+    frontCtrl := &FrontController{}
+    frontGroup := r.Group("/")
+    {
+        frontGroup.GET("", frontCtrl.Index)
+        frontGroup.GET("view", frontCtrl.View)
+    }
+
+    adminCtrl := &AdminController{}
+    adminGroup := r.Group("/admin")
+    store := cookie.NewStore([]byte("secret"))
+    adminGroup.Use(sessions.Sessions("mysession", store))
+    {
+        adminGroup.Static("/assets", "views/admin/assets")
+        adminGroup.GET("", adminCtrl.Index)
+        adminGroup.GET("/login", adminCtrl.Login)
+        adminGroup.POST("/login", adminCtrl.Login)
+        adminGroup.GET("/logout", adminCtrl.Logout)
+        adminGroup.GET("/statistics", adminCtrl.Statistics)
+        adminGroup.GET("/session-test", adminCtrl.SessionTest)
+    }
+    r.Run() // listen and serve on 0.0.0.0:8080
 }
 
-func frontTemplates(templatesDir string, render multitemplate.Renderer) multitemplate.Renderer{
-	tpls := []string{"index","view"}
-	for _, tpl := range tpls {
-		render.AddFromFiles("front/"+tpl, templatesDir+"/"+tpl+".html")
-	}
-	return render
+func frontTemplates(templatesDir string, render multitemplate.Renderer) multitemplate.Renderer {
+    tpls := []string{"index", "view"}
+    for _, tpl := range tpls {
+        render.AddFromFiles("front/"+tpl, templatesDir+"/"+tpl+".html")
+    }
+    return render
 }
 
 func loadTemplates(templatesDir string) multitemplate.Renderer {
-	r := multitemplate.NewRenderer()
+    r := multitemplate.NewRenderer()
 
-	layouts, err := filepath.Glob(templatesDir + "/layouts/*.html")
-	if err != nil {
-		panic(err.Error())
-	}
+    layouts, err := filepath.Glob(templatesDir + "/layouts/*.html")
+    if err != nil {
+        panic(err.Error())
+    }
 
-	includes, err := filepath.Glob(templatesDir + "/includes/*.html")
-	if err != nil {
-		panic(err.Error())
-	}
+    includes, err := filepath.Glob(templatesDir + "/includes/*.html")
+    if err != nil {
+        panic(err.Error())
+    }
 
-	// Generate our templates map from our layouts/ and includes/ directories
-	for _, include := range includes {
-		files := append(layouts, include)
-		r.AddFromFiles(filepath.Base(templatesDir) + "/" + strings.Replace(filepath.Base(include), ".html", "", 1), files...)
-	}
-	return r
+    // Generate our templates map from our layouts/ and includes/ directories
+    for _, include := range includes {
+        files := append(layouts, include)
+        r.AddFromFiles(filepath.Base(templatesDir)+"/"+strings.Replace(filepath.Base(include), ".html", "", 1), files...)
+    }
+    return r
 }
--- framework/gin/demo/models.go
+++ framework/gin/demo/models.go
@@ -2,20 +2,20 @@ package main
 
 import "github.com/jinzhu/gorm"
 
-type Blog struct {
-	gorm.Model
-	Title string `json:"title"`
-	Content string `json:"content"`
-	CatId	string 	`json:"cat_id"`
+type Article struct {
+    gorm.Model
+    Title   string `json:"title"`
+    Content string `json:"content"`
+    CatId   string `json:"cat_id"`
 }
 
-type Category struct{
-	gorm.Model
-	Name string `json:"name"`
+type Category struct {
+    gorm.Model
+    Name string `json:"name"`
 }
 
 type User struct {
-	gorm.Model
-	Username string `json:"username"`
-	Password string `json:"password"`
+    gorm.Model
+    Username string `json:"username"`
+    Password string `json:"password"`
 }
