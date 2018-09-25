6ee3ad9b2e9492e3e5c8cf0053435fc555dbbcbc
walkskyer <walkskyer@qq.com>
2018-09-25 17:16:43 +0800

    gin test gorm BeforeSave 统一

--- framework/gin/demo/models.go
+++ framework/gin/demo/models.go
@@ -1,22 +1,14 @@
 package main
 
 import (
-    "github.com/jinzhu/gorm"
     "time"
 )
-
-type Article struct {
+type MyModel struct{
     ID        uint `gorm:"primary_key"`
-    CreatedAt time.Time `json:"created_at" form:"-"`
-    UpdatedAt time.Time `json:"updated_at" form:"-"`
-    DeletedAt *time.Time `json:"deleted_at" sql:"index" form:"-"`
-    Title   string `json:"title" form:"title" binding:"required"`
-    Content string `json:"content" form:"content" binding:"required"`
-    CatId   string `json:"cat_id" form:"cat_id" `
     Modified int64 `json:"modified" form:"-" gorm:"default:0"`
     Created int64 `json:"created" form:"-" gorm:"default:0"`
 }
-func (m *Article) BeforeSave()error{
+func (m *MyModel) BeforeSave()error{
     m.Modified = time.Now().Unix()
     if m.ID == 0{
         m.Created = m.Modified
@@ -24,13 +16,21 @@ func (m *Article) BeforeSave()error{
     return nil
 }
 
+type Article struct {
+    MyModel
+    Title   string `json:"title" form:"title" binding:"required"`
+    Content string `json:"content" form:"content" binding:"required"`
+    CatId   string `json:"cat_id" form:"cat_id" `
+}
+
+
 type Category struct {
-    gorm.Model
+    MyModel
     Name string `json:"name" form:"name" binding:"required"`
 }
 
 type User struct {
-    gorm.Model
+    MyModel
     Username string `json:"username" gorm:"unique_index"`
     Password string `json:"password"`
 }
