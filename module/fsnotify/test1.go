928a73479662350ede1f34a28247579d2427037d
walkskyer <walkskyer@gmail.com>
2019-07-04 10:15:49 +0800

    fsnotify 重复操作事件处理

--- module/fsnotify/test1.go
+++ module/fsnotify/test1.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
 
 func main() {
+	//watcher, err := fsnotify.NewWatcher()
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -14,6 +16,9 @@ func main() {
 	defer watcher.Close()
 
 	done := make(chan bool)
+
+	var tick = time.Tick(50 * time.Millisecond)
+	var evts = make([]fsnotify.Event, 0)
 	go func() {
 		for {
 			select {
@@ -21,10 +26,19 @@ func main() {
 				log.Println("event:", event)
 				if event.Op&fsnotify.Write == fsnotify.Write {
 					log.Println("[Modified file]:", event.Name)
+					evts = append(evts, event)
 				}
 				if event.Op&fsnotify.Create == fsnotify.Create {
 					log.Println("[Created file]:", event.Name)
+					evts = append(evts, event)
 				}
+			case <-tick:
+				//通过增加时间间隔来过滤编辑器多次操作事件(算是目前比较有效的解决办法)
+				if len(evts) == 0 {
+					continue
+				}
+				log.Println("EVTS:", evts)
+				evts = make([]fsnotify.Event, 0)
 			case err := <-watcher.Errors:
 				log.Println("error:", err)
 			}
