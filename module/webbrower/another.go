fc5b7e9595dc6206e25d9e7e5cd34bf0c411aae1
walkskyer <walkskyer@gmail.com>
2019-07-30 15:52:09 +0800

    使用默认浏览器打开url的另一种方法

---
+++ module/webbrower/another.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+	"runtime"
+)
+
+//open user use default browser
+func main() {
+	OpenBrowser("http://www.baidu.com")
+}
+func OpenBrowser(url string) {
+	var err error
+
+	switch runtime.GOOS {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
