03d4c9db7ae15d402e51d38365d2d273be3a6165
walkskyer <walkskyer@qq.com>
2021-04-15 13:51:50 +0800

    feat(module/gopher-lua):gopher-lua-test callGoModule 多参数处理

--- module/gopher-lua/mymodule/mymodule2.go
+++ module/gopher-lua/mymodule/mymodule2.go
@@ -18,10 +18,39 @@ func Loader2(L *lua.LState) int {
 }
 
 var exports2 = map[string]lua.LGFunction{
-	"myfunc": myfunc2,
+	"myfunc":  myfunc2,
+	"sum_lua": sum2_lua,
 }
 
 func myfunc2(L *lua.LState) int {
 	fmt.Println("call mymodule2 func[myfunc2()] in lua")
 	return 0
+}
+
+func sum2(numbers ...int64) int64 {
+	var sum int64
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+//多参数处理示例
+func sum2_lua(L *lua.LState) int {
+	numbers := make([]int64, 100, 100)
+	i := 1
+	for {
+		lv := L.Get(i)
+		if lv == lua.LNil {
+			break
+		}
+
+		if v, ok := lv.(lua.LNumber); ok {
+			numbers[i-1] = int64(v)
+		}
+		i++
+	}
+	sum := sum2(numbers[:i-1]...)
+	L.Push(lua.LNumber(sum))
+	return 1
 }
