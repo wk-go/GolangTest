00389b3e488ca910ded0ac694dde30f113a1246f
walkskyer <walkskyer@qq.com>
2021-04-16 12:55:54 +0800

    feat(module/gopher-lua):gopher-lua-test codeToShareTest

--- module/gopher-lua/gopher-lua-test.go
+++ module/gopher-lua/gopher-lua-test.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/yuin/gluamapper"
 	lua "github.com/yuin/gopher-lua"
+	"github.com/yuin/gopher-lua/parse"
 	"golang_test/module/gopher-lua/data"
 	"golang_test/module/gopher-lua/mymodule"
 	luar "layeh.com/gopher-luar"
@@ -23,6 +25,7 @@ var (
 		"useGoStruct":       useGoStruct,
 		"useGoStructByLuar": useGoStructByLuar,
 		"gluamapperTest":    gluamapperTest,
+		"codeToShareTest":   codeToShareTest,
 	}
 )
 
@@ -193,4 +196,42 @@ person = {
 		panic(err)
 	}
 	fmt.Printf("Name:%s, Age:%d", person.Name, person.Age)
+}
+
+// CompileLua reads the passed lua file from disk and compiles it.
+func CompileLua(filePath string) (*lua.FunctionProto, error) {
+	file, err := os.Open(filePath)
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+	reader := bufio.NewReader(file)
+	chunk, err := parse.Parse(reader, filePath)
+	if err != nil {
+		return nil, err
+	}
+	proto, err := lua.Compile(chunk, filePath)
+	if err != nil {
+		return nil, err
+	}
+	return proto, nil
+}
+
+// DoCompiledFile takes a FunctionProto, as returned by CompileLua, and runs it in the LState. It is equivalent
+// to calling DoFile on the LState with the original source file.
+func DoCompiledFile(L *lua.LState, proto *lua.FunctionProto) error {
+	lfunc := L.NewFunctionFromProto(proto)
+	L.Push(lfunc)
+	return L.PCall(0, lua.MultRet, nil)
+}
+
+// Example shows how to share the compiled byte code from a lua script between multiple VMs.
+func codeToShareTest() {
+	codeToShare, _ := CompileLua("lua/codeToShare.lua")
+	a := lua.NewState()
+	b := lua.NewState()
+	c := lua.NewState()
+	DoCompiledFile(a, codeToShare)
+	DoCompiledFile(b, codeToShare)
+	DoCompiledFile(c, codeToShare)
 }
---
+++ module/gopher-lua/lua/codeToShare.lua
@@ -0,0 +1,9 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: user007
+-- Date: 2021/4/16
+-- Time: 12:50
+-- To change this template use File | Settings | File Templates.
+--
+
+print("This is a codeToShare example.")
