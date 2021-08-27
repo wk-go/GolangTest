739f8cd7ba3b3405ffdfaeb88bcdee534f6d81cf
Gogs <gogs@fake.local>
2021-08-27 16:26:00 +0800

    amqp rpc queue

---
+++ module/amqp/06_rpc/rpc_server.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"golang_test/module/amqp/util"
+	"log"
+	"strconv"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func fib(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	}
+	n1, n2 := 0, 1
+	tmp := 0
+	for i := 2; i <= n; i++ {
+		tmp = n2
+		n2 = n1 + n2
+		n1 = tmp
+	}
+	return n2
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	util.FailOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	util.FailOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"rpc_queue", // name
+		false,       // durable
+		false,       // delete when unused
+		false,       // exclusive
+		false,       // no-wait
+		nil,         // arguments
+	)
+	util.FailOnError(err, "Failed to declare a queue")
+
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	util.FailOnError(err, "Failed to set QoS")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	util.FailOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			n, err := strconv.Atoi(string(d.Body))
+			util.FailOnError(err, "Failed to convert body to integer")
+
+			log.Printf(" [.] fib(%d)", n)
+			response := fib(n)
+
+			err = ch.Publish(
+				"",        // exchange
+				d.ReplyTo, // routing key
+				false,     // mandatory
+				false,     // immediate
+				amqp.Publishing{
+					ContentType:   "text/plain",
+					CorrelationId: d.CorrelationId,
+					Body:          []byte(strconv.Itoa(response)),
+				})
+			util.FailOnError(err, "Failed to publish a message")
+
+			d.Ack(false)
+		}
+	}()
+
+	log.Printf(" [*] Awaiting RPC requests")
+	<-forever
+}
