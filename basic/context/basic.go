736a99dd0f7d35d95e793148108b7a818a3b85a6
walkskyer <walkskyer@qq.com>
2022-03-11 14:29:35 +0800

    feat(basic/context):上下文基础示例，添加WithValue和WithCancel用法。

--- basic/context/basic.go
+++ basic/context/basic.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
 	"time"
 )
 
@@ -10,18 +10,50 @@ import (
 
 func main() {
 	parentCtx := context.Background()
-	ctx, cancel := context.WithTimeout(parentCtx, 10000*time.Millisecond)
-	defer cancel()
 
+	//with timeout
+	ctx, cancel := context.WithTimeout(parentCtx, 2*time.Second)
+	defer cancel()
 	func(ctx context.Context) {
 		for {
 			select {
-			case <-time.After(1 * time.Second):
-				fmt.Println("overslept")
+			case <-time.After(300 * time.Millisecond):
+				log.Println("Doing something...")
 			case <-ctx.Done():
-				fmt.Println(ctx.Err())
+				log.Println(ctx.Err())
 				return
 			}
 		}
 	}(ctx)
+
+	// with Value
+	withValueFunc := func(ctx context.Context) {
+		val := ctx.Value("name")
+		name := val.(string)
+		log.Println("The name is", name)
+	}
+	ctxWithValue := context.WithValue(parentCtx, "name", "zhang san")
+	withValueFunc(ctxWithValue)
+
+	// with Cancel
+	withCancelFunc := func(ctx context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				log.Println(ctx.Err())
+				return
+			default:
+				log.Println("Working...")
+				time.Sleep(200 * time.Millisecond)
+			}
+		}
+	}
+	ctxWithCancel, cancel2 := context.WithCancel(parentCtx)
+	go withCancelFunc(ctxWithCancel)
+	for i := 0; i < 2; i++ {
+		time.Sleep(time.Second)
+	}
+	cancel2()
+
+	log.Println("Done.")
 }
